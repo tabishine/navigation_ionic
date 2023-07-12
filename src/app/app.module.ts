11bd84d1148810a2ab74220d78415570bcd605d5
Zhambulova Tabigat <zhambulovat@gmail.com>
2023-07-12 17:05:30 +0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,10 @@
+import { NgModule } from '@angular/core';
+import { ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
-
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+
 
 @NgModule({
   declarations: [
@@ -22,9 +21,7 @@ import { HomePage } from '../pages/home/home';
     HomePage
   ],
   providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
-export class AppModule {}
+export class AppModule { }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,18 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  navItems: any[];
 
   constructor(public navCtrl: NavController) {
-
+    this.navItems = [
+      { title: 'Home', page: 'HomePage' },
+      { title: 'About', page: 'AboutPage' },
+      { title: 'Contact', page: 'ContactPage' }
+    ];
   }
 
+  navigate(item: any) {
+    this.navCtrl.push(item.page);
+  }
 }
+
